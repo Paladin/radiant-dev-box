45103fd860676b180fa0de71bd9df84c8d6d7676
Arlen Walker <support@paladinweb.net>
2014-11-10 09:40:53 -0600

    Updated provisioning script
    Added python-software-properties to get add-repository (repo could be manually added to source-list, but that doesn't install the key as well; this is simpler)
    Added the brightbox repo to get access to recent rubies
    Added --force-yes to the apt-get command because otherwise it complains about it if it hits a problem. Ordinarily I'd say "never do this" (and I may yet back this one out) but this is a vagrant dev box so if it really does something nasty it's a simple thing to rebuild it, meanwhile it keeps the provisioning going so you can see what else is wrong.
    Installed ruby 2.1 instead of 1.9 -- added the dev package to compile some extensions, the doc package because I'm anal that way. For the moment passing on the ruby tcl/tk package for now. Not sure if I'm going to need it.
    Changed environment variables for ruby 2.1

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ export DEBIAN_FRONTEND=noninteractive
 echo 'export LANGUAGE="en_US.UTF-8"' >> /etc/profile.d/lang.sh
 echo 'export LANG="en_US.UTF-8"' >> /etc/profile.d/lang.sh
 echo 'export LC_ALL="en_US.UTF-8"' >> /etc/profile.d/lang.sh
-echo 'export GEM_HOME=/home/vagrant/.gem/ruby/1.9.1' >> /etc/profile.d/gems.sh
-echo 'PATH=/home/vagrant/.gem/ruby/1.9.1/bin:$PATH' >> /etc/profile.d/path.sh
+echo 'export GEM_HOME=/home/vagrant/.gem/ruby/2.1' >> /etc/profile.d/gems.sh
+echo 'PATH=/home/vagrant/.gem/ruby/2.1/bin:$PATH' >> /etc/profile.d/path.sh
 echo 'gem: --no-ri --no-rdoc' >> /root/.gemrc
 echo 'gem: --no-ri --no-rdoc' >> /home/vagrant/.gemrc
 
@@ -15,8 +15,12 @@ source /etc/profile.d/gems.sh
 source /etc/profile.d/path.sh
 
 apt-get update
+sudo apt-get install -y --force-yes python-software-properties
+sudo apt-add-repository ppa:brightbox/ruby-ng
 
-apt-get install -y \
+apt-get update
+
+apt-get install -y --force-yes \
 build-essential \
 git \
 htop \
@@ -24,7 +28,9 @@ libxml2-dev libxslt-dev \
 mysql-server mysql-client libmysqlclient-dev \
 nodejs \
 postgresql libpq-dev \
-ruby1.9.1-full \
+ruby2.1 \
+ruby2.1-dev \
+ruby2.1-doc \
 sqlite3 libsqlite3-dev
 
 sudo -u postgres createuser --superuser radiant
